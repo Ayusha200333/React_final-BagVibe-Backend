73479f4c3b3bbc749b099673ec242129fa5d4f5d
Ayusha200333 <ayushawijerathna@gmail.com>
2025-12-20 20:20:34 +0530

    add config

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,23 @@
-const express = require("express")
-const cors = require("cors")
+import express, { Request, Response } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./config/db"
+
+dotenv.config()
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+connectDB()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("WELCOME TO BAGVIBE API")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
